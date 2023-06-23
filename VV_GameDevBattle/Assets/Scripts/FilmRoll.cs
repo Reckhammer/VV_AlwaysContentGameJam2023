886c5a76ba871a80df9edb93eb315b83da72c31a
iirlas <bitwisetuna@gmail.com>
2023-06-23 16:02:30 -0500

    Revised FilmRoll

--- VV_GameDevBattle/Assets/Scripts/FilmRoll.cs
+++ VV_GameDevBattle/Assets/Scripts/FilmRoll.cs
@@ -6,16 +6,20 @@ using UnityEngine.UI;
 public class FilmRoll : MonoBehaviour
 {
     public List<RawImage> images;
-    private List<Texture> textures;
-    private int currentTexture;
+    private List<Texture> textures = new List<Texture>();
+    private int currentTexture = 0;
 
     public void Render(Camera camera)
     {
-        textures.Add(new RenderTexture(camera.activeTexture));
-        (textures[currentTexture] as RenderTexture).Create();
+        if (textures.Count < images.Count)
+        {
+            textures.Add(new RenderTexture(256, 128, 16));
+            (textures[currentTexture] as RenderTexture).Create();
+        }
+
         camera.targetTexture = textures[currentTexture] as RenderTexture;
         camera.Render();
         images[currentTexture].texture = textures[currentTexture];
-        currentTexture++;
+        currentTexture = (currentTexture + 1) % images.Count;
     }
 }
