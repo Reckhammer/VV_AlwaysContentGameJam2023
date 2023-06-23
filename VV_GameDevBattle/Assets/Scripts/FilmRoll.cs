e0edca17047742db14cfb6b8c05bc15f68b52efc
iirlas <bitwisetuna@gmail.com>
2023-06-23 16:38:32 -0500

    Added Flash

--- VV_GameDevBattle/Assets/Scripts/FilmRoll.cs
+++ VV_GameDevBattle/Assets/Scripts/FilmRoll.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class FilmRoll : MonoBehaviour
 {
+    public Light light;
     public List<RawImage> images;
     private List<Texture> textures = new List<Texture>();
     private int currentTexture = 0;
@@ -16,9 +17,11 @@ public class FilmRoll : MonoBehaviour
             textures.Add(new RenderTexture(256, 128, 16));
             (textures[currentTexture] as RenderTexture).Create();
         }
-
+        light.enabled = true;
         camera.targetTexture = textures[currentTexture] as RenderTexture;
         camera.Render();
+        light.enabled = false;
+
         images[currentTexture].texture = textures[currentTexture];
         currentTexture = (currentTexture + 1) % images.Count;
         camera.targetTexture = null;
